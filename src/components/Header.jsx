67e51d54ce80767405fbb8eb4d009da137e082a1
JhonattaSilva2 <jhonatta.ferreira96@outlook.com>
2022-04-06 17:21:33 -0300

    Changed the font-family, craeted the List and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,47 @@
 import styled from "styled-components";
+import LogoImg from "../img/logo.svg";
 
 export default function Header() {
   return (
     <Navbar>
-      <Logo />
+      <Logo src={LogoImg} alt="react-image" />
       <Lista>
-        <Item>About</Item>
-        <Item>Skill</Item>
-        <Item>Projects</Item>
-        <Item>Contatc</Item>
+        <Item>
+          <Link href="#">About</Link>
+        </Item>
+        <Item>
+          <Link href="#">Skills</Link>
+        </Item>
+        <Item>
+          <Link href="#">Projects</Link>
+        </Item>
+        <Item>
+          <Link href="#">Contact</Link>
+        </Item>
       </Lista>
     </Navbar>
   );
 }
 
 const Navbar = styled.nav`
-background-color: blue;
-display: flex;
-min-width: 100%;
+  background-color: red;
+  display: flex;
+  justify-content: space-between;
+  min-width: 100%;
 `;
 
-const Logo = styled.img``;
+const Logo = styled.img`
+  max-width: 52px;
+`;
 
-const Lista = styled.ul``;
-const Item = styled.li``;
+const Lista = styled.ul`
+  display: flex;
+  min-width: 50%;
+  justify-content: space-around;
+`;
+
+const Item = styled.li`
+list-style-type: none;
+`;
+
+const Link = styled.a``;
