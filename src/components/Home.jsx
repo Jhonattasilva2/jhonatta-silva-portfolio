211fed27397445815ae4d940b323d4e241547160
JhonattaSilva2 <jhonatta.ferreira96@outlook.com>
2022-04-06 21:02:25 -0300

    Improved the Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,26 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export default function Home() {
   return (
-    <Main>Teste</Main>
-  )
+    <Main>
+      <NameDiv></NameDiv>
+      <PhotoDiv></PhotoDiv>
+    </Main>
+  );
 }
 
 const Main = styled.main`
+  min-width: 100%;
+  min-height: 100vh;
+  display: flex;
+`;
 
-`
+const NameDiv = styled.div`
+background-color: red;
+min-width: 50%;
+`;
+
+const PhotoDiv = styled.div`
+background-color: green;
+min-width: 50%;
+`;
