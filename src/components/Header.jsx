2446886859613ffe12d1389d7f335d9a9475bff2
JhonattaSilva2 <jhonatta.ferreira96@outlook.com>
2022-04-07 20:08:59 -0300

    Installed the React-icons and finished the Name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,12 +38,15 @@ const Logo = styled.img`
 
 const Lista = styled.ul`
   display: flex;
-  min-width: 50%;
+  min-width: 40%;
   justify-content: space-around;
 `;
 
 const Item = styled.li`
   list-style-type: none;
+  display: block;
+  height: 100%;
+  
 `;
 
 const Link = styled.a`
@@ -52,6 +55,7 @@ const Link = styled.a`
   color: #fff;
   font-size: 1.2rem;
   transition: 0.4s ease;
+  
 
   &:hover {
     color: #000;
